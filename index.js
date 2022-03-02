f6f2a178309994db44ce1f7c2af183ecb346e268
adrian-palacios <adrian.palacios0403@gmail.com>
2022-03-01 23:37:01 -0600

    used cheerio and succesfully scraped title and url

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const express = require("express");
 const app = express();
 const url = "https://www.theguardian.com/us";
 
-axios(url).then((response) => {
-	const html = response.data;
-	console.log(html);
-});
+axios(url)
+	.then((response) => {
+		const html = response.data;
+		// $ essentially uses all the html, similar to jquery
+		const $ = cheerio.load(html);
+		const articles = [];
+		$(".fc-item__title", html).each(function () {
+			const title = $(this).text();
+			// a tag needed to get the correct href
+			const url = $(this).find("a").attr("href");
+			articles.push({ title, url });
+		});
+		console.log(articles);
+	})
+	.catch((error) => console.log(error));
 
 app.listen(PORT, () => console.log("the server is running on PORT 8000"));
