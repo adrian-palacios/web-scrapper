fc9d47de6c747b4b857c1065220853f833bd3488
adrian-palacios <adrian.palacios0403@gmail.com>
2022-03-09 17:07:12 -0600

    used express to get scraped data on localhost

--- index.js
+++ index.js
@@ -6,20 +6,26 @@ const express = require("express");
 const app = express();
 const url = "https://www.theguardian.com/us";
 
-axios(url)
-	.then((response) => {
-		const html = response.data;
-		// $ essentially uses all the html, similar to jquery
-		const $ = cheerio.load(html);
-		const articles = [];
-		$(".fc-item__title", html).each(function () {
-			const title = $(this).text();
-			// a tag needed to get the correct href
-			const url = $(this).find("a").attr("href");
-			articles.push({ title, url });
-		});
-		console.log(articles);
-	})
-	.catch((error) => console.log(error));
+app.get("/", (req, res) => {
+	res.json("Welcome to my webscraper");
+});
+
+app.get("/info", (req, res) => {
+	axios(url)
+		.then((response) => {
+			const html = response.data;
+			// $ essentially uses all the html, similar to jquery
+			const $ = cheerio.load(html);
+			const articles = [];
+			$(".fc-item__title", html).each(function () {
+				const title = $(this).text();
+				// a tag needed to get the correct href
+				const url = $(this).find("a").attr("href");
+				articles.push({ title, url });
+			});
+			res.json(articles);
+		})
+		.catch((error) => console.log(error));
+});
 
 app.listen(PORT, () => console.log("the server is running on PORT 8000"));
